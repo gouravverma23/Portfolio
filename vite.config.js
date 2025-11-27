408a1cecf5da7019a5d26c486764836015c71528
gouravverma23 <gauravverma@23042006@gmail.com>
2025-11-27 18:27:26 +0530

    Remove from github

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/Portfolio/', // runs without /Portfolio/
-  plugins: [react()],
+export default defineConfig({ 
 })
