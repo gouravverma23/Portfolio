922c13a5ef1a15cbc81a23c5d0d8ac90247addd6
Gourav Verma <gauravverma23042006@gmail.com>
2026-08-10 22:46:45 +0530

    Fix some issues

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,5 +1,5 @@
 import { FaJs, FaPython, FaJava, FaHtml5, FaReact, FaNodeJs, FaGitAlt, FaDatabase, FaChartBar, FaCss3 } from 'react-icons/fa';
-import { SiCplusplus, SiMysql, SiTailwindcss, SiExpress, SiSupabase, SiMongodb, SiVscodium, SiVercel, SiGooglecolab, SiNumpy, SiPandas, SiScikitlearn } from 'react-icons/si';
+import { SiCplusplus, SiMysql, SiTailwindcss, SiExpress,SiPostgresql, SiSupabase, SiMongodb, SiVercel, SiNumpy, SiPandas, SiScikitlearn, SiRender } from 'react-icons/si';
 
 export const skills = [
   {
@@ -11,11 +11,11 @@ export const skills = [
         icon: FaPython,
         subItems: ["Tkinter", "NumPy", "Pandas", "Matplotlib", "Seaborn", "Scikit-learn"]
       },
-      { name: "Java", icon: FaJava },
+      { name: "Java (Beginner)", icon: FaJava },
       {
         name: "SQL",
         icon: FaDatabase,
-        subItems: ["MySQL"]
+        subItems: ["MySQL","PostgreSQL"]
       }
     ]
   },
@@ -36,24 +36,20 @@ export const skills = [
       { name: "Express js", icon: SiExpress },
       { name: "Supabase", icon: SiSupabase },
       { name: "MongoDB", icon: SiMongodb },
-      { name: "MySQL", icon: SiMysql }
+      { name: "PostgreSQL", icon: SiPostgresql},
     ]
   },
   {
     category: "Tools",
     items: [
-      {
-        name: "Code Editor & IDE",
-        icon: SiVscodium,
-        subItems: ["VScode", "Intellij", "Jupyter Notebook", "Antigravity"]
-      },
+      // {
+      //   name: "Code Editor & IDE",
+      //   icon: SiVscodium,
+      //   subItems: ["VScode", "Intellij", "Jupyter Notebook", "Antigravity"]
+      // },
       { name: "Git and Github", icon: FaGitAlt },
-      {
-        name: "Frontend Deployment",
-        icon: SiVercel,
-        subItems: ["Github Pages", "Vercel"]
-      },
-      { name: "Google colab", icon: SiGooglecolab }
+      { name: "Vercel", icon: SiVercel },
+      { name: "Render", icon: SiRender}
     ]
   },
   {
@@ -61,7 +57,6 @@ export const skills = [
     items: [
       { name: "Numpy", icon: SiNumpy },
       { name: "Pandas", icon: SiPandas },
-      { name: "Matplotlib & Seaborn (data visualization)", icon: FaChartBar },
       { name: "Scikit-learn", icon: SiScikitlearn },
     ]
   }
